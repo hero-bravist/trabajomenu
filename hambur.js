50f68b461ba29aaf44c5b302edac61c68ffe887b
hero-bravist <108357186+hero-bravist@users.noreply.github.com>
2022-09-09 10:14:35 -0500

    Add files via upload

---
+++ hambur.js
@@ -0,0 +1,10 @@
+function hamburguesa(){
+    let hambur = document.getElementById('sel1').value
+    let bebida = document.getElementById('sel2').value
+    let postre = document.getElementById('sel3').value
+    let personas = document.getElementById('persona-uno').value
+    let nombre = document.getElementById('nombre-uno').value
+    let direccion = document.getElementById('direccion-uno').value
+    
+    
+}
