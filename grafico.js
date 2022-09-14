ec8b5830a94b389ed0450105625d447b0fee48a9
hero-bravist <108357186+hero-bravist@users.noreply.github.com>
2022-09-13 20:59:09 -0500

    Add files via upload

--- grafico.js
+++ grafico.js
@@ -1,25 +1,56 @@
-function generarGrafico(){
-    let espacio = document.getElementById('grafico').value;
-    let valor1 = document.getElementById('valor1').value;
-    let valor2 = document.getElementById('valor2').value;
-    let valor3 = document.getElementById('valor3').value;
-    let valor4 = document.getElementById('valor4').value;
+// function generarGrafico(){
+//     let espacio = document.getElementById('grafico').value;
+//     let valor1 = document.getElementById('valor1').value;
+//     let valor2 = document.getElementById('valor2').value;
+//     let valor3 = document.getElementById('valor3').value;
+//     let valor4 = document.getElementById('valor4').value;
 
-    plotly.newplot(espacio, [{
-        x: [valor1, valor3],
-        y: [valor2, valor4]}]);
+//     plotly.newplot(espacio, [{
+//         x: [valor1, valor3],
+//         y: [valor2, valor4]}]);
 
     // var data = [{
     //     x: [valor1, valor3]
     // }]
-}
-function grafico(){
-    let espacio = document.getElementById('grafi').value;
-    let valo1  = document.getElementById('valuno').value;
-    let valo2 = document.getElementById('valdos').value;
-    let valo3 = document.getElementById('valtres').value;
 
-    plotly.newplot(grafi, [{
-        x: [valo1, valo2, valo3],
-        y: []}]);
+// function grafico(){
+//     let espacio = document.getElementById('grafi').value;
+//     let valo1  = document.getElementById('valuno').value;
+//     let valo2 = document.getElementById('valdos').value;
+//     let valo3 = document.getElementById('valtres').value;
+
+//     plotly.newplot(grafi, [{
+//         x: [valo1, valo2, valo3],
+//         y: []}]);
+// }
+function graficodos(){
+    let espacio = document.getElementById('grafi');
+    let espacios = document.getElementById('grafi2');
+    let valor1  = document.getElementById('valuno').value;
+    let valor2 = document.getElementById('valdos').value;
+    let valor3 = document.getElementById('valtres').value;
+    var data = [{
+        values: [valor1, valor2, valor3],
+        labels: ['saisfaccion', 'como le parecio', 'nivel de satisfaccion'],
+        type: 'pie'
+      }];
+      
+      var layout = {
+        height: 400,
+        width: 500
+      };
+      
+      Plotly.newPlot( espacio, data, layout);
+
+      var trace1 = {
+        x: ['saisfaccion', 'como le parecio', 'nivel de satisfaccion'],
+        y: [valor1, valor2, valor3],
+        type: 'scatter'
+      };
+      
+
+      
+      var data = [trace1];
+      
+      Plotly.newPlot(espacios, data);
 }
--- hambur.js
+++ hambur.js
@@ -2,21 +2,45 @@ function hamburguesa(){
     let hambur = document.getElementById('sel1').value
     let bebida = document.getElementById('sel2').value
     let postre = document.getElementById('sel3').value
-    let personas = document.getElementById('persona-uno').value
+    let personas = document.getElementById('personas-uno').value
     let nombre = document.getElementById('nombre-uno').value
     let direccion = document.getElementById('direccion-uno').value
     
     var documentPDF = new jsPDF();
-    documentPDF.setfontsize(20);
+    documentPDF.setFontSize(20);
     documentPDF.text(20,20, 'reporte de ventas');
-    documentPDF.setfontsize(12);
-    documentPDF.text(20,30, 'fecha:' + new date());
-    documentPDF.setfontsize(10);
-    documentPDF.text(20,50, 'hamburguesa:' + sel1 );
-    documentPDF.text(20,60, 'bebida:' + sel2 );
-    documentPDF.text(20,70, 'postre:' + sel3 );
+    documentPDF.setFontSize(12);
+    documentPDF.text(20,30, 'fecha:' + new Date());
+    documentPDF.setFontSize(10);
+    documentPDF.text(20,50, 'hamburguesa:' + hambur );
+    documentPDF.text(20,60, 'bebida:' + bebida );
+    documentPDF.text(20,70, 'postre:' + postre );
     documentPDF.text(20,80, 'personas:' + personas );
     documentPDF.text(20,90, 'Nombre:' + nombre );
     documentPDF.text(20,100, 'direccion:' + direccion );
     documentPDF.save('archivo.PDF');
+
+}
+function perro(){
+    let hambur = document.getElementById('sel1').value
+    let bebida = document.getElementById('sel2').value
+    let postre = document.getElementById('sel3').value
+    let personas = document.getElementById('personas-uno').value
+    let nombre = document.getElementById('nombre-uno').value
+    let direccion = document.getElementById('direccion-uno').value
+    
+    var documentPDF = new jsPDF();
+    documentPDF.setFontSize(20);
+    documentPDF.text(20,20, 'reporte de ventas');
+    documentPDF.setFontSize(12);
+    documentPDF.text(20,30, 'fecha:' + new Date());
+    documentPDF.setFontSize(10);
+    documentPDF.text(20,50, 'perro:' + hambur );
+    documentPDF.text(20,60, 'bebida:' + bebida );
+    documentPDF.text(20,70, 'postre:' + postre );
+    documentPDF.text(20,80, 'personas:' + personas );
+    documentPDF.text(20,90, 'Nombre:' + nombre );
+    documentPDF.text(20,100, 'direccion:' + direccion );
+    documentPDF.save('archivo.PDF');
+
 }
